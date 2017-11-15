5f90bd19a652af711d489d5b0e35ea0eb665696a
gabor-szeles <szelesgbr@gmail.com>
2017-11-15 11:41:51 +0100

    first working version

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,2 +1,31 @@
+import java.util.LinkedList;
+
 public class Algorithm {
+
+    private static LinkedList<Integer> tinkerTailor(int N, int k){
+        LinkedList<Integer> initList = new LinkedList<>();
+        LinkedList<Integer> result = new LinkedList<>();
+        int targetNumber;
+        generateList(N, initList);
+        int popIdx = 0;
+        while (!initList.isEmpty()) {
+            popIdx = ((popIdx + k) - 1) % initList.size();
+            targetNumber = initList.get(popIdx);
+            result.add(targetNumber);
+            initList.remove(popIdx);
+        }
+        return result;
+    }
+
+    private static void generateList(int N, LinkedList<Integer> numberList) {
+        for (Integer i =1;i<=N;i++) {
+            numberList.add(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> toPrint;
+        toPrint = tinkerTailor(5000, 3);
+        System.out.println(toPrint);
+    }
 }
